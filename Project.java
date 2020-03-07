1805189c27058d44b9262e3bcbf3cdea91ca48f3
melsamaniego <59934207+melsamaniego@users.noreply.github.com>
2020-03-07 11:25:37 -0700

    Update Project.java

--- Project.java
+++ Project.java
@@ -21,5 +21,21 @@ public class Project {
     
   }
   
+  public static void printUsage() {
+    System.out.println("java Project /?");
+    System.out.println("java Project CreateItem <itemCode> <itemDescription> <price>");
+    System.out.println("java Project CreatePurchase <itemCode> <PurchaseQuantity>");
+    System.out.println("java Project CreateShipment <itemCode> <ShipmentQuantity> <shipmentDate>");
+    System.out.println("java Project GetItems <itemCode>");
+    System.out.println("java Project GetShipments <itemCode>");
+    System.out.println("java Project GetPurchases <itemCode>");
+    System.out.println("java Project ItemsAvailable <itemCode>"); //most complext requirement
+    System.out.println("java Project UpdateItem <itemCode> <price>");
+    System.out.println("java Project DeleteItem <itemCode>");
+    System.out.println("java Project DeleteShipment <itemCode>");
+    System.out.println("java Project DeletePurchase <itemCode>");
+    
+  }
+  
  
 }
