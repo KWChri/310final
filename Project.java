5ad9c6ad297188c4ad68cd98a63b7bc482f7be52
melsamaniego <59934207+melsamaniego@users.noreply.github.com>
2020-03-18 22:34:32 -0600

    Update Project.java

--- Project.java
+++ Project.java
@@ -21,6 +21,50 @@ public class Project {
     
   }
   
+  public static void CreateItem (String itemCode, String itemDescription, String price) {
+    
+  }
+  
+  public static void CreatePurchase (String itemCode, int purchaseQuantity) {
+    
+  }
+  
+  public static void CreateShipment (String itemCode, int shipmentQuantity, date shipmentDate) {
+    
+  }
+  
+  public static void GetItems (String itemCode) {
+    
+  }
+  
+  public static void GetShipments (String itemCode) {
+    
+  }
+  
+  public static void GetPurchases (String itemCode) {
+    
+  }
+  
+  public static void ItemsAvailable (String itemcCode) {
+    
+  }
+  
+  public static void UpdateItem (String itemCode, String price) {
+    
+  }
+  
+  public static void DeleteItem (String itemCode) {
+    
+  }
+  
+  public static void DeleteShipment (String itemCode) {
+    
+  }
+  
+  public static void DeletePurchase (String itemCode) {
+    
+  }
+  
   public static void printUsage() {
     System.out.println("java Project /?");
     System.out.println("java Project CreateItem <itemCode> <itemDescription> <price>");
